c1860e0518f7f2fd18269fd6193677e0a885aa3e
osmane bns <osmanebns@github.com>
2021-09-20 00:29:57 +0100

    task 2

--- task2 UnSupervised ML.py
+++ task2 UnSupervised ML.py
@@ -1,6 +1,29 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
 import numpy as np
+
+iris = pd.read_csv('Iris.csv')
+print(iris.shape)
+
+samples=iris.iloc[:,[1,2,3,4]].values
+print(samples)
+optimum_set = []
+for k in range(1,9):
+    model = KMeans(n_clusters=k )
+    model.fit(samples)
+    optimum_set.append(model.inertia_)
+# print(optimum_set)
+ks=range(1,9)
+plt.plot(ks, optimum_set, '-o')
+plt.xlabel('number of clusters, k')
+plt.ylabel('inertia')
+plt.xticks(ks)
+plt.show()
+print(optimum_set)
+
+model_opt=KMeans(n_clusters=3)
+model_opt.fit(samples)
+labels=model_opt.predict()
+
